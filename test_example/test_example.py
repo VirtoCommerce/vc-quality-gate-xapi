0788b6e98aee4cd7961e5ebcb23b0da27f214dbb
ElenaMutykova <elena.mutykova@virtoway.com>
2024-11-27 12:53:32 +0100

    Add installer and metadata files for various packages in the virtual environment

---
+++ test_example/test_example.py
@@ -0,0 +1,13 @@
+import pytest
+
+from playwright.sync_api import sync_playwright
+
+def run(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://example.com")
+    print(page.title())  # Prints the title of the page
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
